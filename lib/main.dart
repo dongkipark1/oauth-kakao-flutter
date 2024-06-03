7415c7c5a4968b9eea8507706e18111ad9f4b3e5
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-06-03 16:25:19 +0900

    oauth 크리덴셜 플러터

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
+
+
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(
+    nativeAppKey: 'd8979ed8e5433f262229c4b30bdc967a'
+    //   javaScriptAppKey: 'c28c3165a482a6733e22867d06809da7'
+  );
   runApp(ProviderScope(child: MyApp()));
 }
 
